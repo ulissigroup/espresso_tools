6fb94b4e26a51545ffadd7a66d2611b13d2c5f94
Kevin Tran <ktrain9891@gmail.com>
2019-11-21 09:45:20 -0500

    Finish getting read of threads per core

--- gaspy_wrappers/core.py
+++ gaspy_wrappers/core.py
@@ -81,7 +81,6 @@ def run_job():
     manager = settings['manager']
     nodes = settings['nodes']
     cores_per_node = settings['cores_per_node']
-    threads_per_core = settings['threads_per_core']
     pw_executable = settings['qe_executable']
 
     # Use heuristics to trim down run conditions for small systems
@@ -96,9 +95,9 @@ def run_job():
 
     # Call the HPC-specific command to actually run
     if manager == 'slurm':
-        _run_on_slurm(nodes, cores_per_node, threads_per_core, pw_executable)
+        _run_on_slurm(nodes, cores_per_node, pw_executable)
     elif manager == 'lsf':
-        _run_on_lsf(nodes, cores_per_node, threads_per_core, pw_executable)
+        _run_on_lsf(nodes, cores_per_node, pw_executable)
     else:
         raise RuntimeError('espresso_tools does not yet know how to submit '
                            'jobs to the "%s" manager. Please modify '
